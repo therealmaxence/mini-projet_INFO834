b67d8e92e91c8e0347e7f8bda05b1e8c28ebc6ad
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2026-03-27 15:56:13 +0100

    socket server OK

--- websocket/src/index.js
+++ websocket/src/index.js
@@ -1,42 +1,63 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const jwt = require('jsonwebtoken');
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import jwt from 'jsonwebtoken';
+
 
 const app = express();
-const httpServer = http.createServer(app);
+const server = createServer(app);
 
-const io = new Server(httpServer, {
+const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ['GET', 'POST'],
   },
 });
 
-io.use((socket, next) => {
-  const token = socket.handshake.auth.token;
-  if (!token) return next(new Error('AUTH_REQUIRED'));
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    socket.user = decoded;
-    next();
-  } catch (err) {
-    next(new Error('Authentication error'));
-  }
+app.get('/', (req, res) => {
+  res.send(`
+    <script src="/socket.io/socket.io.js"></script>
+    <script>
+      const socket = io();
+    </script>
+  `);
 });
+
+// io.use((socket, next) => {
+//   const token = socket.handshake.auth.token;
+//   if (!token) return next(new Error('AUTH_REQUIRED'));
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     socket.user = decoded;
+//     next();
+//   } catch (err) {
+//     next(new Error('Authentication error'));
+//   }
+// });
 
 
 io.on('connection', (socket) => {
-  console.log(`[+] ${socket.user.username} connecté (${socket.id})`);
+  console.log('connection');
+  // console.log(`[+] ${socket.user.username} connecté (${socket.id})`);
+
+  socket.on('message', (msg) => {
+    console.log(msg);
+    // console.log(`[MSG] ${socket.user.username}: ${msg}`);
+    // io.emit('message', { user: socket.user.username, text: msg });
+  });
 
   socket.on('disconnecting', () => {
-    console.log(`[~] ${socket.user.username} est entrain d'être déconnecté (${socket.id})`);
+    console.log('disconnecting');
+    // console.log(`[~] ${socket.user.username} est entrain d'être déconnecté (${socket.id})`);
   });
 
   socket.on('disconnect', () => {
-    console.log(`[-] ${socket.user.username} déconnecté (${socket.id})`);
+    console.log('disconnect');
+    // console.log(`[-] ${socket.user.username} déconnecté (${socket.id})`);
   });
 });
 
-httpServer.listen(3001, () => console.log('🚀 Server ready on :3001'));
+server.listen(3001, () => {
+  console.log('Server running on :3001')
+});
